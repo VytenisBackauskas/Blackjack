c35d78d36cfa078cac904e56f37451f934396830
VytenisBackauskas <vytenis.backauskas@gmail.com>
2021-10-12 18:49:12 +0300

    Created working game.

---
+++ Blackjack/GameHandler.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blackjack
+{
+    class GameHandler
+    {
+        private CardPack cardPack;
+        private Random cardPicker;
+
+        private Player player;
+        private Dealer dealer;
+
+        public GameHandler()
+        {
+            this.cardPack = new CardPack(2);
+            this.cardPicker = new Random();
+            this.player = new Player();
+            this.dealer = new Dealer();
+        }
+
+        public void PrintInfo(string user)
+        {
+            if(user == "player")
+            {
+                player.PrintInfo();
+            }
+            else if(user == "dealer")
+            {
+                dealer.PrintInfo();
+            }
+        }
+
+        public void DrawCard(string user)
+        {
+            if (user == "player")
+            {
+                cardPack = player.DrawCard(cardPack, cardPicker);
+            }
+            else if (user == "dealer")
+            {
+                cardPack = dealer.DrawCard(cardPack, cardPicker);
+            }
+        }
+
+        public void DrawAce(string user)
+        {
+            if (user == "player")
+            {
+                player.DrawAce();
+            }
+            else if (user == "dealer")
+            {
+                dealer.DrawAce();
+            }
+        }
+
+        public void CheckAce()
+        {
+            player.CheckAce();
+            dealer.CheckAce();
+        }
+
+        public int CheckBlackjack()
+        {
+            if(player.CheckBlackjack() && dealer.CheckBlackjack())
+            {
+                Tie();
+                return 1;
+            }
+            else if (player.CheckBlackjack())
+            {
+                Win();
+                return 2;
+            }
+            else if (dealer.CheckBlackjack())
+            {
+                Lose();
+                return 3;
+            }
+            return 0;
+        }
+
+        public int CheckGameState()
+        {
+            int blackjackState = CheckBlackjack();
+            if (dealer.getCardSum() == player.getCardSum())
+            {
+                Tie();
+                return 1;
+            }
+            else if (blackjackState == 2 || (player.getCardSum() <= 21 && player.getCardSum() > dealer.getCardSum()) || player.getCardSum() <= 21 && dealer.getCardSum() > 21)
+            {
+                Win();
+                return 2;
+            }
+            else
+            {
+                Lose();
+                return 0;
+            }
+        }
+
+        public void Win()
+        {
+            Console.WriteLine("Laimėjote");
+        }
+
+        public void Lose()
+        {
+            Console.WriteLine("Pralaimėjote");
+        }
+
+        public void Tie()
+        {
+            Console.WriteLine("Lygiosios");
+        }
+
+        public bool CanDraw(string user)
+        {
+            if (user == "player")
+            {
+                return player.CanDraw();
+            }
+            else if (user == "dealer")
+            {
+                return dealer.CanDraw();
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ Blackjack/Player.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blackjack
+{
+    class Player
+    {
+        private List<char> cards;
+        int cardSum;
+        int aceUses;
+
+        public Player()
+        {
+            this.cards = new List<char>();
+            this.cardSum = 0;
+            this.aceUses = 0;
+        }
+
+        public void PrintInfo()
+        {
+            Console.WriteLine("Žaidėjo kortos: ");
+            foreach (char card in this.cards)
+            {
+                Console.Write("{0} ", card);
+            }
+            Console.WriteLine("- {0}", this.cardSum);
+        }
+
+        public CardPack DrawCard(CardPack cardPack, Random cardPicker)
+        {
+            this.cards.Add(cardPack.GetCards()[cardPicker.Next(0, cardPack.GetCards().Count)]);
+            cardPack.GetCards().Remove(this.cards[this.cards.Count - 1]);
+            this.cardSum += cardPack.GetCardValues()[this.cards[this.cards.Count - 1]];
+            return cardPack;
+        }
+
+        public bool CheckAce()
+        {
+            if (cardSum <= 21)
+            {
+                return false;
+            }
+            int aceCards = 0;
+            foreach (char card in this.cards)
+            {
+                if (card == 'A')
+                {
+                    aceCards++;
+                }
+            }
+            if (aceCards > aceUses)
+            {
+                this.cardSum -= 10;
+                aceUses++;
+                return true;
+            }
+            return false;
+        }
+
+        public bool CheckBlackjack()
+        {
+            if(cardSum == 21)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool CanDraw()
+        {
+            if(cardSum < 21)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public int getCardSum()
+        {
+            return this.cardSum;
+        }
+
+        public void DrawAce()
+        {
+            this.cards.Add('A');
+            this.cardSum += 11;
+        }
+    }
+}
