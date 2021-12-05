f962c75b61a2f6a4473de0e5b8c100c483aaef83
VytenisBackauskas <85632493+VytenisBackauskas@users.noreply.github.com>
2021-12-05 14:52:22 +0200

    Introduced Null Object design pattern in GameEntity class

---
+++ Blackjack/GameEntities/NullGameEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blackjack.GameEntities
+{
+    class NullGameEntity : GameEntity
+    {
+        public override void PrintInfo()
+        {
+            Console.WriteLine("Tai yra null objektas, su juo neturėtų būti dirbama!");
+        }
+    }
+}
--- Blackjack/GameProcesses/GameLauncher.cs
+++ Blackjack/GameProcesses/GameLauncher.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using Blackjack.GameEntities;
 using BlackjackGameState;
 
 namespace Blackjack
